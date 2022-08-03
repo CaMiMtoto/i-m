77e48c9cadba9fa726c738fea93ea91b44f29197
Jean Paul Byiringiro <jeanpaulcami@live.com>
2022-08-03 11:49:59 +0200

    redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
     content: ["./src/**/*.{html,js}", "./**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                brand: {
+
+                    "500": "#003281"
+                }
+            }
+        },
     },
     plugins: [],
     important: true,
